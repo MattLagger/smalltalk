3aafc5c2c9822933ff4155c64b73e55a66fda6d3
Wendreo Matheus <wendreo.pvh@gmail.com>
2022-12-07 08:54:42 -0300

    created a server

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+//how to create a express api with socket.io using typescript?
+import * as express from "express";
+import * as http from "http";
+import * as socketio from "socket.io";
+
+const app = express.default();
+
+app.get("/", (_req, res) => {
+  res.send({ uptime: process.uptime() });
+});
+
+const server = http.createServer(app);
+const io = new socketio.Server(server);
+
+io.on("connection", (...params) => {
+  console.log(params);
+});
+
+server.listen(4004, () => {
+  console.log("Running at localhost:4004");
+});
+
+
+
+
